8b24acd0de678018a19790c36670a841039f1030
Muhammad Hamza <codingtimelapseyt@gmail.com>
2026-01-19 23:20:59 -0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,65 +5,80 @@ import Image from "next/image";
 import Navbar from "@/components/navbar";
 import Container from "@/components/container";
 
-const projects = [
+const years = [
   {
-    name: "UmmahHacks",
-    description: "Jan 24, 2026",
-    image: "/ummahhacks.jpg",
-    url: "https://ummahhacks.com",
+    year: 2025,
+    things: [
+      {
+        name: "UmmahHacks",
+        description: "Jan 24, 2026",
+        image: "/ummahhacks.jpg",
+        url: "https://ummahhacks.com",
+      },
+      {
+        name: "Muhammad Hamza",
+        description: "Portfolio Website",
+        image: "/mh.svg",
+      },
+      {
+        name: "Shopify",
+        description: "Merchant Authentication",
+        image: "/shopify.jpg",
+        url: "https://shopify.com",
+      },
+    ],
   },
   {
-    name: "Muhammad Hamza",
-    description: "Portfolio Website",
-    image: "/mh.svg",
+    year: 2024,
+    things: [
+      {
+        name: "falcon",
+        description: "Internship Dashboard",
+        image: "/falcon.png",
+        url: "https://tryfalcon.io",
+      },
+      {
+        name: "Hack the North",
+        description: "AI Fit Checker (Top 32)",
+        image: "/hack-the-north.jpg",
+        url: "https://hackthenorth.com",
+      },
+      {
+        name: "Vidyard",
+        description: "AI Avatar Features",
+        image: "/vidyard.png",
+        url: "https://vidyard.com",
+      },
+      {
+        name: "Nokia",
+        description: "Autonomous Networks",
+        image: "/nokia.jpg",
+        url: "https://nokia.com",
+      },
+    ],
   },
   {
-    name: "Shopify",
-    description: "Merchant Authentication",
-    image: "/shopify.jpg",
-    url: "https://shopify.com",
-  },
-  {
-    name: "falcon",
-    description: "Internship Dashboard",
-    image: "/falcon.png",
-    url: "https://tryfalcon.io",
-  },
-  {
-    name: "Hack the North",
-    description: "AI Fit Checker (Top 32)",
-    image: "/hack-the-north.jpg",
-    url: "https://hackthenorth.com",
-  },
-  {
-    name: "Vidyard",
-    description: "AI Avatar Features",
-    image: "/vidyard.png",
-    url: "https://vidyard.com",
-  },
-  {
-    name: "Nokia",
-    description: "Autonomous Networks",
-    image: "/nokia.jpg",
-    url: "https://nokia.com",
-  },
-  {
-    name: "UWPlanr",
-    description: "Course Planner",
-    image: "/uwplanr.jpg",
-    url: "https://uwplanr.netlify.app",
-  },
-  {
-    name: "Twimbit",
-    description: "Telco AI/CX Research",
-    image: "/twimbit.jpg",
-    url: "https://tryfalcon.io",
-  },
-  {
-    name: "University of Waterloo",
-    description: "Computer Science (C&O Minor)",
-    image: "/university-of-waterloo.jpeg",
-    url: "https://uwaterloo.ca",
+    year: 2023,
+    things: [
+      {
+        name: "UWPlanr",
+        description: "Course Planner",
+        image: "/uwplanr.jpg",
+        url: "https://uwplanr.netlify.app",
+      },
+      {
+        name: "Twimbit",
+        description: "Telco AI/CX Research",
+        image: "/twimbit.jpg",
+        url: "https://tryfalcon.io",
+      },
+      {
+        name: "University of Waterloo",
+        description: "Computer Science (C&O Minor)",
+        image: "/university-of-waterloo.jpeg",
+        url: "https://uwaterloo.ca",
+      },
+    ],
   },
 ];
 
@@ -73,25 +88,39 @@ export default function Home() {
       <Navbar />
 
       <div className="space-y-4">
-        {projects.map((project) => (
-          <Link
-            target={project.url && "_blank"}
-            key={project.name}
-            href={project.url ? project.url : "/"}
-            className="flex items-start p-2 rounded-full gap-2 transition-all duration-300 hover:bg-[#FAF9F5] hover:shadow-2xl"
-          >
-            <Image
-              className="rounded-full"
-              src={project.image}
-              alt={project.name}
-              width={50}
-              height={50}
-            />
-            <div className="flex-1 min-w-0">
-              <h3 className="font-medium mb-1">{project.name}</h3>
-              <p className="text-[#5E5D59] text-sm">{project.description}</p>
+        {years.map((year, index) => (
+          <>
+            <h2 className="relative flex items-center">
+              <span className="absolute left-0 right-0 top-1/2 h-[0.5px] bg-[#5E5D59]"></span>
+              <span className="relative bg-[#e8eded] pr-4 text-lg font-semibold">
+                {year.year}
+              </span>
+            </h2>
+            <div className="space-y-4">
+              {year.things.map((project) => (
+                <Link
+                  target={project.url && "_blank"}
+                  key={project.name}
+                  href={project.url ? project.url : "/"}
+                  className="flex items-start p-2 rounded-full gap-2"
+                >
+                  <Image
+                    className="rounded-full"
+                    src={project.image}
+                    alt={project.name}
+                    width={50}
+                    height={50}
+                  />
+                  <div className="flex-1 min-w-0">
+                    <h3 className="font-medium mb-1">{project.name}</h3>
+                    <p className="text-[#5E5D59] text-sm">
+                      {project.description}
+                    </p>
+                  </div>
+                </Link>
+              ))}
             </div>
-          </Link>
+          </>
         ))}
       </div>
 
--- components/container.tsx
+++ components/container.tsx
@@ -4,7 +4,7 @@ export default function Container({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="min-h-screen bg-[#F0EEE6] text-[#141413] flex items-center justify-center p-6">
+    <main className="min-h-screen bg-[#e8eded] text-[#141413] flex items-center justify-center p-6">
       <div className="max-w-lg w-full py-12">{children}</div>
     </main>
   );
