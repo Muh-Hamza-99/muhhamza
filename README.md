c4511070c49ec629580c3d42e9aead7b8c5f689b
Muhammad Hamza <codingtimelapseyt@gmail.com>
2026-01-30 00:25:00 -0500

    new simple

--- README.md
+++
@@ -1,7 +0,0 @@
-# muhhamza
-
-My portfolio website.
-
-# tech
-
-![tech](https://skillicons.dev/icons?i=tailwind,ts,react,nextjs,netlify)
--- components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="mt-8 text-sm text-[#5E5D59]">
-      <p>© {new Date().getFullYear()} Muhammad Hamza</p>
-    </div>
-  );
-}
