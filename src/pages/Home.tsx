7575a8abe5b20b974b4e08264f4b8fb1fa9c669c
Muhammad Hamza <codingtimelapseyt@gmail.com>
2026-05-18 08:08:09 -0400

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import Courses from "../components/boxes/courses";
 import Experiences from "../components/boxes/experiences";
+import Extracurriculars from "../components/boxes/extracurriculars";
 import Hero from "../components/boxes/hero";
 import Projects from "../components/boxes/projects";
 
 const Home = () => {
   return (
     <main className="min-h-screen flex items-center bg-background text-foreground font-sans">
-      <div className="w-full mx-auto max-w-4xl px-4 md:px-8 lg:px-16 xl:px-32">
+      <div className="w-full mx-auto max-w-3xl px-4 md:px-8 lg:px-16 xl:px-32">
         <Hero />
         <Experiences />
         <Projects />
         <Courses />
+        <Extracurriculars />
       </div>
     </main>
   );
