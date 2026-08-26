c1831d122e4ff8a84ecf394ed29232b8b6336f92
Muhammad Hamza <94744748+Muh-Hamza-99@users.noreply.github.com>
2026-08-26 15:56:41 -0400

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,7 @@ import { PROJECTS } from "../lib/data";
 const Projects = () => {
   return (
     <Section>
-      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+      <div className="grid grid-cols-2 gap-3">
         {PROJECTS.map((project, index) => (
           <Card key={index} project={project} />
         ))}
